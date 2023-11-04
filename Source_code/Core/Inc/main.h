180838d3d2cc757814189c6299f08435172da600
hungpham1406 <hung.pham14062003@hcmut.edu.vn>
2023-11-04 18:11:02 +0700

    Init port and pin for project

--- Source_code/Core/Inc/main.h
+++ Source_code/Core/Inc/main.h
@@ -58,14 +58,52 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
-#define Button1_Pin GPIO_PIN_13
-#define Button1_GPIO_Port GPIOC
-#define LED_RED_Pin GPIO_PIN_5
-#define LED_RED_GPIO_Port GPIOA
-#define LED_YELLOW_Pin GPIO_PIN_6
-#define LED_YELLOW_GPIO_Port GPIOA
-#define LED_GREEN_Pin GPIO_PIN_7
-#define LED_GREEN_GPIO_Port GPIOA
+#define LED_RED1_Pin GPIO_PIN_1
+#define LED_RED1_GPIO_Port GPIOA
+#define LED_YELLOW1_Pin GPIO_PIN_2
+#define LED_YELLOW1_GPIO_Port GPIOA
+#define LED_GREEN1_Pin GPIO_PIN_3
+#define LED_GREEN1_GPIO_Port GPIOA
+#define LED_RED2_Pin GPIO_PIN_4
+#define LED_RED2_GPIO_Port GPIOA
+#define LED_YELLOW2_Pin GPIO_PIN_5
+#define LED_YELLOW2_GPIO_Port GPIOA
+#define LED_GREEN2_Pin GPIO_PIN_6
+#define LED_GREEN2_GPIO_Port GPIOA
+#define a2_Pin GPIO_PIN_0
+#define a2_GPIO_Port GPIOB
+#define b2_Pin GPIO_PIN_1
+#define b2_GPIO_Port GPIOB
+#define c2_Pin GPIO_PIN_2
+#define c2_GPIO_Port GPIOB
+#define button3_Pin GPIO_PIN_10
+#define button3_GPIO_Port GPIOB
+#define a1_Pin GPIO_PIN_9
+#define a1_GPIO_Port GPIOA
+#define b1_Pin GPIO_PIN_10
+#define b1_GPIO_Port GPIOA
+#define c1_Pin GPIO_PIN_11
+#define c1_GPIO_Port GPIOA
+#define d1_Pin GPIO_PIN_12
+#define d1_GPIO_Port GPIOA
+#define e1_Pin GPIO_PIN_13
+#define e1_GPIO_Port GPIOA
+#define f1_Pin GPIO_PIN_14
+#define f1_GPIO_Port GPIOA
+#define g1_Pin GPIO_PIN_15
+#define g1_GPIO_Port GPIOA
+#define d2_Pin GPIO_PIN_3
+#define d2_GPIO_Port GPIOB
+#define e2_Pin GPIO_PIN_4
+#define e2_GPIO_Port GPIOB
+#define f2_Pin GPIO_PIN_5
+#define f2_GPIO_Port GPIOB
+#define g2_Pin GPIO_PIN_6
+#define g2_GPIO_Port GPIOB
+#define button1_Pin GPIO_PIN_8
+#define button1_GPIO_Port GPIOB
+#define button2_Pin GPIO_PIN_9
+#define button2_GPIO_Port GPIOB
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
--- Source_code/Core/Src/main.c
+++ Source_code/Core/Src/main.c
@@ -199,24 +199,46 @@ static void MX_GPIO_Init(void)
   GPIO_InitTypeDef GPIO_InitStruct = {0};
 
   /* GPIO Ports Clock Enable */
-  __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, LED_RED_Pin|LED_YELLOW_Pin|LED_GREEN_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, LED_RED1_Pin|LED_YELLOW1_Pin|LED_GREEN1_Pin|LED_RED2_Pin
+                          |LED_YELLOW2_Pin|LED_GREEN2_Pin|a1_Pin|b1_Pin
+                          |c1_Pin|d1_Pin|e1_Pin|f1_Pin
+                          |g1_Pin, GPIO_PIN_RESET);
 
-  /*Configure GPIO pin : Button1_Pin */
-  GPIO_InitStruct.Pin = Button1_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-  GPIO_InitStruct.Pull = GPIO_PULLUP;
-  HAL_GPIO_Init(Button1_GPIO_Port, &GPIO_InitStruct);
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, a2_Pin|b2_Pin|c2_Pin|d2_Pin
+                          |e2_Pin|f2_Pin|g2_Pin, GPIO_PIN_RESET);
 
-  /*Configure GPIO pins : LED_RED_Pin LED_YELLOW_Pin LED_GREEN_Pin */
-  GPIO_InitStruct.Pin = LED_RED_Pin|LED_YELLOW_Pin|LED_GREEN_Pin;
+  /*Configure GPIO pins : LED_RED1_Pin LED_YELLOW1_Pin LED_GREEN1_Pin LED_RED2_Pin
+                           LED_YELLOW2_Pin LED_GREEN2_Pin a1_Pin b1_Pin
+                           c1_Pin d1_Pin e1_Pin f1_Pin
+                           g1_Pin */
+  GPIO_InitStruct.Pin = LED_RED1_Pin|LED_YELLOW1_Pin|LED_GREEN1_Pin|LED_RED2_Pin
+                          |LED_YELLOW2_Pin|LED_GREEN2_Pin|a1_Pin|b1_Pin
+                          |c1_Pin|d1_Pin|e1_Pin|f1_Pin
+                          |g1_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : a2_Pin b2_Pin c2_Pin d2_Pin
+                           e2_Pin f2_Pin g2_Pin */
+  GPIO_InitStruct.Pin = a2_Pin|b2_Pin|c2_Pin|d2_Pin
+                          |e2_Pin|f2_Pin|g2_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : button3_Pin button1_Pin button2_Pin */
+  GPIO_InitStruct.Pin = button3_Pin|button1_Pin|button2_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
 }
 
